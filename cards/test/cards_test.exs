d93f87f4226bf786b74b4115cbbec4c03593a44c
Soren Rood <sorenrood@gmail.com>
2020-04-01 13:10:36 -0700

    added cards files

---
+++ cards/test/cards_test.exs
@@ -0,0 +1,17 @@
+defmodule CardsTest do
+  use ExUnit.Case
+  doctest Cards
+
+  test "create deck makes 20 cards" do 
+
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 20
+  end
+
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck
+    assert deck != Cards.shuffle(deck)
+  end
+
+
+end
